c2da9dc25cacc9645d8873713b4ff9080584850f
Adam Labi <labi722@gmail.com>
2021-04-13 15:07:47 -0400

    another one

--- index.js
+++ index.js
@@ -12,9 +12,15 @@ try {
   core.info(title)
   const toBeChecked = regex.test(title)
   if (!toBeChecked) return;
+  const octokit = github.getOctokit(token);
+  const response = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews', {
+    owner: github.context.payload.organization.login,
+    repo: github.context.payload.repository.name,
+    pull_number: github.context.payload.number
+  })
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(response, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
