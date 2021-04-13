03c131607594902916a701c7e96ec250d7a62fac
Adam Labi <labi722@gmail.com>
2021-04-13 14:34:34 -0400

    first pass

---
+++ index.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const regex = core.getInput('regex');
+  const numApprovalsRequired = core.getInput('num-approvals-required');
+  const token = core.getInput('token');
+  const title =
+    github.context.payload &&
+    github.context.payload.pull_request &&
+    github.context.payload.pull_request.title
+  core.info(title)
+  const toBeChecked = regex.test(title)
+  if (!toBeChecked) return;
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
